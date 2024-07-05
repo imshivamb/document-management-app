58e7e1cfb5cccbe92ae995587d5c5a90b55f32da
Shivam Bhardwaj <98094474+imshivamb@users.noreply.github.com>
2024-07-05 22:47:58 +0530

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,8 +5,6 @@ export default defineConfig({
     dialect: "postgresql",
     schema: './src/lib/db.ts',
     out: './drizzle',
-    dbCredentials: {
-        url: 'postgresql://neondb_owner:6dp7AQTIgCVP@ep-ancient-sun-a5ovaik2.us-east-2.aws.neon.tech/neondb?sslmode=require',
-      }
+    
     
 })
